7d5d74739a48a6ae434a888b12d476119c05c5a3
Varga József Márk <vmark2145@gmail.com>
2022-04-03 20:04:30 +0200

    Initial import

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module hu.unideb.inf.homeworkproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens hu.unideb.inf.homeworkproject to javafx.fxml;
+    exports hu.unideb.inf.homeworkproject;
+}
