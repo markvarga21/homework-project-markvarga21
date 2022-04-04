e1a3fd5590f6438a3f7e422e5c20cd216caffd72
Varga József Márk <vmark2145@gmail.com>
2022-04-04 22:00:30 +0200

    Adding documentation to Orientation enum.

--- src/main/java/hu/unideb/inf/homeworkproject/model/Orientation.java
+++ src/main/java/hu/unideb/inf/homeworkproject/model/Orientation.java
@@ -1,5 +1,22 @@
 package hu.unideb.inf.homeworkproject.model;
 
+/**
+ * An enum representing the player's selection orientation, which can be either {@code HORIZONTAL} or {@code VERTICAL}
+ * or {@code NOT_CALIBRATED} (which is used if the player had selected only one node - the first node in particular).
+ */
 public enum Orientation {
-    HORIZONTAL, VERTICAL, NOT_CALIBRATED
+    /**
+     * Player selection made in a {@code HORIZONTAL} way, so the circles are in a row.
+     */
+    HORIZONTAL,
+
+    /**
+     * Player selection made in a {@code VERTICAL} way, so the circles are in a row.
+     */
+    VERTICAL,
+
+    /**
+     * It is used when a player selects the first circle.
+     */
+    NOT_CALIBRATED
 }
