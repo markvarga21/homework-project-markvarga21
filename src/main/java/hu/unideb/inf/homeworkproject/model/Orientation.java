55beffea7148bfa6222f9c1e0409865f2221bb32
Varga József Márk <vmark2145@gmail.com>
2022-04-05 18:38:05 +0200

    Correcting documentation of Orientation enum, and also creating the package-info for model package.

--- src/main/java/hu/unideb/inf/homeworkproject/model/Orientation.java
+++ src/main/java/hu/unideb/inf/homeworkproject/model/Orientation.java
@@ -1,17 +1,21 @@
 package hu.unideb.inf.homeworkproject.model;
 
 /**
- * An enum representing the player's selection orientation, which can be either {@code HORIZONTAL} or {@code VERTICAL}
- * or {@code NOT_CALIBRATED} (which is used if the player had selected only one node - the first node in particular).
+ * An enum representing the player's selection orientation,
+ * which can be either {@code HORIZONTAL} or {@code VERTICAL}
+ * or {@code NOT_CALIBRATED} (which is used if the player
+ * had selected only one node - the first node in particular).
  */
 public enum Orientation {
     /**
-     * Player selection made in a {@code HORIZONTAL} way, so the circles are in a row.
+     * Player selection made in a {@code HORIZONTAL} way,
+     * so the circles are in a row.
      */
     HORIZONTAL,
 
     /**
-     * Player selection made in a {@code VERTICAL} way, so the circles are in a row.
+     * Player selection made in a {@code VERTICAL} way,
+     * so the circles are in a row.
      */
     VERTICAL,
 
---
+++ src/main/java/hu/unideb/inf/homeworkproject/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * In {@code model} package there are classes which are
+ * operating on datas, such as board field statuses, validation,
+ * and helping classes.
+ */
+package hu.unideb.inf.homeworkproject.model;
