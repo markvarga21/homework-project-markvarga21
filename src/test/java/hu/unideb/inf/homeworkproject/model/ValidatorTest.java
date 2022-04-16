0d6905eb206d7da775170535168a036d36a8497e
Varga József Márk <vmark2145@gmail.com>
2022-04-16 12:10:39 +0200

    Starting to implement ValidatorTest.

---
+++ src/test/java/hu/unideb/inf/homeworkproject/model/ValidatorTest.java
@@ -0,0 +1,32 @@
+package hu.unideb.inf.homeworkproject.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+
+    @Test
+    void isValidSelection() {
+    }
+
+    @Test
+    void testIsValidSelection() {
+    }
+
+    @Test
+    void getOrientation() {
+    }
+
+    @Test
+    void checkRowInterference() {
+    }
+
+    @Test
+    void checkColumnInterference() {
+    }
+
+    @Test
+    void checkWinner() {
+    }
+}
