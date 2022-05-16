77998985fa9d380ce2e9de6577a20f9ac92aeafc
Varga József Márk <vmark2145@gmail.com>
2022-05-16 20:06:30 +0200

    Added package-info to controller and server packages.

---
+++ src/main/java/hu/unideb/inf/homeworkproject/controller/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The {@code controller} package is used to control and also
+ * manipulate the user interface and the model too. It handles user
+ * input too.
+ */
+package hu.unideb.inf.homeworkproject.controller;
---
+++ src/main/java/hu/unideb/inf/homeworkproject/server/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package represents the data storing and loading too,
+ * like updating player scores (leaderboards), saving game states
+ * and loading them back.
+ */
+package hu.unideb.inf.homeworkproject.server;
